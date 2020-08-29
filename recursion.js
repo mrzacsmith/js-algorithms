acc1fe3b06b3d7146fec869e886c643b26d2c8cd
Zac Smith <zrs3141592@gmail.com>
2020-08-29 11:56:14 -0600

    complete 1

---
+++ recursion.js
@@ -0,0 +1,11 @@
+// 1: Rewrite the factorial function using recursion
+// Time complexity = O(n)
+
+// const fact = (n) => {
+//   if (n === 1) return 1
+//   return n * fact(n - 1)
+// }
+
+// console.log(fact(3))
+// console.log(fact(4))
+// console.log(fact(5))
