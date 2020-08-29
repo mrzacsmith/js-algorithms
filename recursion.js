88077c2795abc19480d0aad8bbb226249be35a84
Zac Smith <zrs3141592@gmail.com>
2020-08-29 13:04:56 -0600

    discuss using count to count executions

--- recursion.js
+++ recursion.js
@@ -1,3 +1,24 @@
+// Review of fibonacci using recursion
+// Time complexity: there are 9 function calls for fib(4), 15 function calls for fib(5), 25 function calls for fib(6)   [1,1,2,3,5,8,13,21]
+// loop-based O(n)
+// fib(4) = 5   => 9
+// fib(5) = 8   => 15 + 6
+// fib(6) = 13  => 25 + 10
+// fib(7) = 21  => 41 + 16
+// fib(8) = 34  => 67 + 26
+// fib(9) = 55  => 109 + 43
+// fib(10)= 89  => 177 + 67
+
+// let count = 0
+// const fib = (n) => {
+//   count++
+//   if (n === 0 || n === 1) return 1
+
+//   return fib(n - 1, count) + fib(n - 2, count)
+// }
+
+// console.log('=', fib(30), 'count:', count)
+
 // 1: Rewrite the factorial function using recursion
 // Time complexity = O(n)
 
