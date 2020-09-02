8035ec1d7aa7a0ceade55b1ed4ac5d9df24bd5ff
Zac Smith <zrs3141592@gmail.com>
2020-09-02 11:37:04 -0600

    iterative binary search

--- search.js
+++ search.js
@@ -1,5 +1,26 @@
 // 1: liner search fucntion before review, O(n) worst case
+//
 // const array = [3, 10, -3, 48, 5, 33, 99]
+let numArray = [-1, 0, 3, 4, 9, 12]
+
+const bSearch = (nums, target) => {
+  let left = 0
+  let right = nums.length - 1
+
+  while (left <= right) {
+    const mid = left + Math.floor((right - left) / 2)
+    if (nums[mid] === target) {
+      return mid
+    } else if (nums[mid] < target) {
+      left = mid + 1
+    } else {
+      right = mid - 1
+    }
+  }
+  return -1
+}
+
+console.log(bSearch(numArray, 9))
 
 // const linSearch = (n, arr) => {
 //   console.log(n, array) //1
