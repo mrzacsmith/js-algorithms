47e9ff6c25f38fbedef643996cedce23ae24b68e
Zac Smith <zrs3141592@gmail.com>
2020-09-02 12:27:28 -0600

    differnt approach to binary search

--- search.js
+++ search.js
@@ -1,26 +1,46 @@
 // 1: liner search fucntion before review, O(n) worst case
 //
 // const array = [3, 10, -3, 48, 5, 33, 99]
+
 let numArray = [-1, 0, 3, 4, 9, 12]
 
-const bSearch = (nums, target) => {
-  let left = 0
-  let right = nums.length - 1
+const bSearch = (arr, target) => {
+  let startInd = 0
+  let endInd = arr.length - 1
 
-  while (left <= right) {
-    const mid = left + Math.floor((right - left) / 2)
-    if (nums[mid] === target) {
-      return mid
-    } else if (nums[mid] < target) {
-      left = mid + 1
-    } else {
-      right = mid - 1
+  while (startInd <= endInd) {
+    const midInd = Math.floor((endInd - startInd) / 2)
+
+    if (target === arr[midInd]) {
+      return midInd
+    }
+
+    if (arr[midInd] < target) {
+      startInd
     }
   }
-  return -1
 }
 
 console.log(bSearch(numArray, 9))
+
+// const bSearch = (nums, target) => {
+//   let left = 0
+//   let right = nums.length - 1
+
+//   while (left <= right) {
+//     const mid = left + Math.floor((right - left) / 2)
+//     if (nums[mid] === target) {
+//       return mid
+//     } else if (nums[mid] < target) {
+//       left = mid + 1
+//     } else {
+//       right = mid - 1
+//     }
+//   }
+//   return -1
+// }
+
+// console.log(bSearch(numArray, 9))
 
 // const linSearch = (n, arr) => {
 //   console.log(n, array) //1
