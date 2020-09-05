2948437288460dbed9a347165f40a380546d9d8d
Zac Smith <zrs3141592@gmail.com>
2020-09-04 22:31:48 -0600

    big O analysis

--- sorting.js
+++ sorting.js
@@ -1,35 +1,41 @@
+// 2: Quicksort
+
 // 1: Bubble sort
+// Time compexity
+//    Best case: sorted and O(n)
+//    Average case:
+//    Worst case: O(n^2) ~~ nested loops without reducing, 1 loop O(n), 2 loops O(n^2), 3 loops O(n^3)
 
-const sort = (arr) => {
-  const resultArray = [...arr]
-  for (let outer = 0; outer < resultArray.length; outer++) {
-    let outerElement = resultArray[outer]
+// const sort = (arr) => {
+//   const resultArray = [...arr]
+//   for (let outer = 0; outer < resultArray.length; outer++) {
+//     let outerElement = resultArray[outer]
 
-    for (let inner = outer + 1; inner < resultArray.length; inner++) {
-      let innerElement = resultArray[inner]
-      console.log('OUTER LOOP')
-      console.log(outerElement)
-      if (outerElement > innerElement) {
-        console.log('INNER LOOP')
-        console.log('outerElement', outerElement)
-        console.log('innerElement', innerElement)
-        resultArray[outer] = innerElement
-        resultArray[inner] = outerElement
-        // console.log(resultArray)
+//     for (let inner = outer + 1; inner < resultArray.length; inner++) {
+//       let innerElement = resultArray[inner]
+//       console.log('OUTER LOOP')
+//       console.log(outerElement)
+//       if (outerElement > innerElement) {
+//         console.log('INNER LOOP')
+//         console.log('outerElement', outerElement)
+//         console.log('innerElement', innerElement)
+//         resultArray[outer] = innerElement
+//         resultArray[inner] = outerElement
+//         // console.log(resultArray)
 
-        outerElement = resultArray[outer]
-        innerElement = resultArray[inner]
-        console.log('INSIDE IF')
-        console.log(resultArray)
-      }
-      console.log('END OF INNER LOOP')
-      console.log(resultArray)
-    }
-  }
+//         outerElement = resultArray[outer]
+//         innerElement = resultArray[inner]
+//         console.log('INSIDE IF')
+//         console.log(resultArray)
+//       }
+//       console.log('END OF INNER LOOP')
+//       console.log(resultArray)
+//     }
+//   }
 
-  return resultArray
-}
+//   return resultArray
+// }
 
-// const sortedArray = sort([5, -10, 10, -3, -1, 20, 100, 19])
-const sortedArray = sort([5, 10, -3])
-console.log(sortedArray)
+// // const sortedArray = sort([5, -10, 10, -3, -1, 20, 100, 19])
+// const sortedArray = sort([5, 10, -3])
+// console.log(sortedArray)
