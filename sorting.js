3e8dcde70f93e7311f03669e756e3ee0ce5e90b4
Zac Smith <zrs3141592@gmail.com>
2020-09-04 23:07:10 -0600

    big o analysis of quicksort

--- sorting.js
+++ sorting.js
@@ -1,51 +1,61 @@
+// 3: merge sort
+
 // 2: Quicksort (uses recursion to improve Big O)
+// Time compexity
+//    Best case: randomly sorted O(n * log n)
+//    Average case: randomly sorts O(n *log n)
+//    Worst case: sorted O(n^2)
+// recursive call is O(n) but if you loop inside of the recursive call O(n^2)
+// Recursive step runtime: O(n^logb(a)) => a = 2, b = 2 => O(n^ log2(2)) => 0(n)
+// Runtime outside of loop: O(n)
+// Algorithm runtime: O(n^logb(a) * log n) => O(n * log n)
 
-const sort = (array) => {
-  const copiedArray = [...array]
-  console.log('FUNCTION STARTING')
-  console.log(array)
+// const sort = (array) => {
+//   const copiedArray = [...array]
+//   console.log('FUNCTION STARTING')
+//   console.log(array)
 
-  if (copiedArray.length <= 1) {
-    console.log('ONLY ONE ITEM - RETURNING', array)
-    return copiedArray
-  }
+//   if (copiedArray.length <= 1) {
+//     console.log('ONLY ONE ITEM - RETURNING', array)
+//     return copiedArray
+//   }
 
-  const smallerElementsArray = []
-  const largerElementsArray = []
-  const pivotElement = copiedArray.shift()
-  const centerElementsArray = [pivotElement]
+//   const smallerElementsArray = []
+//   const largerElementsArray = []
+//   const pivotElement = copiedArray.shift()
+//   const centerElementsArray = [pivotElement]
 
-  while (copiedArray.length) {
-    const currentElement = copiedArray.shift()
-    if (currentElement === pivotElement) {
-      centerElementsArray.push(currentElement)
-    } else if (currentElement < pivotElement) {
-      smallerElementsArray.push(currentElement)
-    } else {
-      largerElementsArray.push(currentElement)
-    }
-  }
+//   while (copiedArray.length) {
+//     const currentElement = copiedArray.shift()
+//     if (currentElement === pivotElement) {
+//       centerElementsArray.push(currentElement)
+//     } else if (currentElement < pivotElement) {
+//       smallerElementsArray.push(currentElement)
+//     } else {
+//       largerElementsArray.push(currentElement)
+//     }
+//   }
 
-  console.log('AFTER WHILE LOOP')
-  console.log(smallerElementsArray)
-  console.log(centerElementsArray)
-  console.log(largerElementsArray)
-  const smallerElementsSortedArray = sort(smallerElementsArray)
-  const largerElementsSortedArray = sort(largerElementsArray)
-  return smallerElementsSortedArray.concat(
-    centerElementsArray,
-    largerElementsSortedArray
-  )
-}
+//   console.log('AFTER WHILE LOOP')
+//   console.log(smallerElementsArray)
+//   console.log(centerElementsArray)
+//   console.log(largerElementsArray)
+//   const smallerElementsSortedArray = sort(smallerElementsArray)
+//   const largerElementsSortedArray = sort(largerElementsArray)
+//   return smallerElementsSortedArray.concat(
+//     centerElementsArray,
+//     largerElementsSortedArray
+//   )
+// }
 
 // const sortedArray = sort([5, -10, 10, -3, -1, 20, 100, -3, 19])
-const sortedArray = sort([5, -3, 10, -3, -15])
-console.log(sortedArray)
+// const sortedArray = sort([5, -3, 10, -3, -15])
+// console.log(sortedArray)
 
 // 1: Bubble sort
 // Time compexity
 //    Best case: sorted and O(n)
-//    Average case:
+//    Average case: O(n^2)
 //    Worst case: O(n^2) ~~ nested loops without reducing, 1 loop O(n), 2 loops O(n^2), 3 loops O(n^3)
 
 // const sort = (arr) => {
