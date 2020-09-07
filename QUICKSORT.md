a24e5d4c74f528c8ab0068bd5a1922546692c314
Zac Smith <zrs3141592@gmail.com>
2020-09-07 17:21:43 -0600

    quicksort and notes

--- QUICKSORT.md
+++ QUICKSORT.md
@@ -5,4 +5,8 @@ Uses a pivot, first index and then creates a smaller chunk array, pivot or cente
 [ ] create sort function that takes in an array parameter
 [ ] copy the array to copiedArray = [...arr]
 [ ] use if to check copiedArray.length as an exit mechanism to end recursion
-[ ] construct arrays for smallerElementsArray, biggerElementsArray, and centerElementArray and the pivot item which is first index using shift()
+[ ] construct arrays for smallerElementsArray, biggerElementsArray, and centerElementArray and the pivot item which is first index using shift() and assign that value to centerElementsArray[]
+[ ] use a while loop while copiedArray.length is truthy. Each itereation will slice off first index, each iteraton getting smaller.
+[ ] create a currentElement assigned copiedArray.shift()
+[ ] create if / else if / else to check if currentElement == pivotElement => centerElementsArray.push(currentElemet), the >, < >
+[ ] create 2 new arrays that = the sort(smallerElementsArray) and finallay return smallerSorted and concat.(center, bigger)
--- quicksort.js
+++ quicksort.js
@@ -2,16 +2,32 @@ const cRA = require('mzs-randomarray')
 const arrayToBeSorted = cRA(40)
 console.table(arrayToBeSorted)
 
-// const sort = arr => {
-//   let copiedArray = [...arr]
+const sort = (arr) => {
+  let copiedArray = [...arr]
 
-//   if (copiedArray.length <= 1) return copiedArray
+  if (copiedArray.length <= 1) return copiedArray
 
-//   const smallerElementsArray = []
-//   const centerElementsArray = []
-//   const biggerElementsArray = []
-//   const pivotElements = copiedArray.shift()
+  const smallerElementsArray = []
+  const biggerElementsArray = []
+  const pivotElement = copiedArray.shift()
+  const centerElementsArray = [pivotElement]
+  while (copiedArray.length) {
+    const currentElement = copiedArray.shift()
+    if (currentElement === pivotElement) {
+      centerElementsArray.push(currentElement)
+    } else if (currentElement < pivotElement) {
+      smallerElementsArray.push(currentElement)
+    } else {
+      biggerElementsArray.push(currentElement)
+    }
+  }
 
-// }
+  const smallerElementsSortedArray = sort(smallerElementsArray)
+  const biggerElementsSortedArray = sort(biggerElementsArray)
+  return smallerElementsSortedArray.concat(
+    centerElementsArray,
+    biggerElementsSortedArray
+  )
+}
 
-// console.log(sort(arrayToBeSorted));
+console.log(sort(arrayToBeSorted))
