7d4e5aff3a09e229308d94fc9f3b1eb23bded929
Zac Smith <zrs3141592@gmail.com>
2020-09-09 00:29:37 -0600

    use set to remove duplicates

--- sets.js
+++ sets.js
@@ -1,4 +1,6 @@
 // A Set is a collection of values (objects, numbers) which forms an entity itself.
+// The Set object lets you store unique values of any type, whether primitive values or object references.
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
 
 // 2: Cartesian products - More than 2 sets
 //  Time complexity
@@ -44,3 +46,15 @@ const colors = ['blue', 'red', 'green']
 const sizes = ['s', 'm', 'l', 'xl']
 const styles = ['round neck', 'v neck']
 // console.log(cartProducts(colors, sizes))
+
+let stuffSet = new Set()
+
+stuffSet.add(2)
+stuffSet.add(3)
+console.log(stuffSet)
+console.log(stuffSet.has(5))
+console.log(stuffSet.has(3))
+console.log(stuffSet.size)
+
+const numberArray = [2, 3, 4, 4, 2, 3, 5, 6, 6, 7, 4, 5, 7, 8, 8, 9, 1, 2, 1]
+console.log([...new Set(numberArray)])
